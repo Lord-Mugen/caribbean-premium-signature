a292453e58a9d188337e6f299b9a9a85a9fe44f0
Christian ruiz <christianjavieruizcortes@gmail.com>
2023-10-05 12:33:14 -0500

    images compression

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import Hero from "./hero/Hero.jsx";
-import Slider_1 from "./slider_1/Slider_1.jsx";
-import Slider_2 from "./slider_2/Slider_2.jsx";
-import CTA from "./cta/CTA.jsx";
-
-export { Hero, Slider_1, Slider_2, CTA };
